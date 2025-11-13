64d06d59cd1fae2a17002b21ccb1f6573fc99e23
RenovZ <cgsixv@gmail.com>
2025-11-13 13:54:28 +0800

    fix: empty doc comments

--- rust/scripts/openapi.sh
+++ rust/scripts/openapi.sh
@@ -2,6 +2,10 @@
 
 openapi-generator-cli generate -c config.yaml
 
-cargo clippy --fix
+rg -l '^\s*///\s*$' src | while read -r file; do
+    perl -i -ne 'print unless /^\s*\/\/\/\s*$/;' "$file"
+done
+
+cargo clippy --fix --allow-dirty --allow-staged
 
 cargo fmt --all
