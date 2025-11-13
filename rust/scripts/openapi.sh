f4f34d20e869076c5e25b2de22631f6309c60fd0
RenovZ <cgsixv@gmail.com>
2025-11-13 15:03:15 +0800

    fix: openapi generation script

--- rust/scripts/openapi.sh
+++ rust/scripts/openapi.sh
@@ -6,6 +6,12 @@ rg -l '^\s*///\s*$' src | while read -r file; do
     perl -i -ne 'print unless /^\s*\/\/\/\s*$/;' "$file"
 done
 
+LINTS=("unused_variables" "dead_code" "unused_imports")
+for lint in "${LINTS[@]}"; do
+    grep -q '#!\[allow('"$lint"')\]' src/lib.rs || \
+    perl -i -0pe "s/\A/#![allow($lint)]\n/" src/lib.rs
+done
+
 cargo clippy --fix --allow-dirty --allow-staged
 
 cargo fmt --all
